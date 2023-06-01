c6442e51b5cfe73889e3b57c69acd199ae9fc5ac
Arnol Alexander Guevara Ramos <arnolpacman@gmail.com>
2023-06-01 11:22:52 -0400

    updated

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,22 +1,16 @@
 import { Global, Module } from '@nestjs/common';
 import { UsersController } from '@users/controllers/users.controller';
 import { UsersService } from '@users/services/users.service';
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserEntity } from "@users/models/entities/user.entity";
-import { ClubsModule } from "@clubs/clubs.module";
-import { UserRepository } from './models/repositories/user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '@users/models/entities/user.entity';
+import { ClubsModule } from '@clubs/clubs.module';
+import { UserRepository } from '@users/models/repositories/user.repository';
 
 @Global()
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([
-            UserEntity,
-        ]),
-        ClubsModule,
-    ],
-    controllers: [ UsersController ],
-    providers: [ UserRepository, UsersService ],
-    exports: [ UsersService ],
+  imports: [TypeOrmModule.forFeature([UserEntity]), ClubsModule],
+  controllers: [UsersController],
+  providers: [UserRepository, UsersService],
+  exports: [UsersService],
 })
-export class UsersModule {
-}
+export class UsersModule {}
